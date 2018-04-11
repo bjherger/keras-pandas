631dbd0f437b21b15d0aa5ff467b13bf361b708a
Brendan Herger <brendanherger@Brendans-MacBook-Pro.local>
2018-04-11 14:41:44 -0700

    Updating gitignore for pypi related files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='auto_dl',
+    version='0.1',  # Update the version number for new releases
+    url='https://github.com/bjherger/auto_dl',
+    author='Brendan Herger',
+    author_email='13herger@gmail.com',
+    license='MIT',
+    packages=['auto_dl'],
+    zip_safe=False
+)
