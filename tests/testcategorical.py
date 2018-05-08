f983292335158cae2a9390667a91f20fbc03cda2
Brendan Herger <13herger@gmail.com>
2018-05-07 20:31:44 -0700

    Adding outline for categorical tests

---
+++ tests/testcategorical.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class TestCategorical(unittest.TestCase):
+
+    def test_fit(self):
+        pass
+
+    def test_transform(self):
+        pass
+
+    def test_create_input_nub_numerical(self):
+        pass
+
+    def test_numerical_whole(self):
+        pass
+
+    @staticmethod
+    def iris_dataframe():
+        return pandas.read_csv('test_data/iris.csv')
