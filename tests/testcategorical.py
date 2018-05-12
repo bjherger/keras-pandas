b3eaab2eb705bfaf2a7f44a98687614d046a94de
Brendan Herger <13herger@gmail.com>
2018-05-11 17:10:26 -0700

    Adding tests for transform w/o response available

--- tests/testcategorical.py
+++ tests/testcategorical.py
@@ -12,12 +12,12 @@ from keras_pandas.Automater import Automater
 class TestCategorical(unittest.TestCase):
 
     def test_fit(self):
-        test_df = self.mushroom_dataframe()
+        train_df = self.mushroom_dataframe()
 
         # Two variables
         mushroom_categorical_cols = ['odor', 'habitat']
         auto = Automater(categorical_vars=mushroom_categorical_cols)
-        auto.fit(test_df)
+        auto.fit(train_df)
 
         self.assertEqual(Automater, type(auto))
         self.assertEqual(mushroom_categorical_cols, auto._user_provided_variables)
@@ -26,42 +26,70 @@ class TestCategorical(unittest.TestCase):
         # Assert that transformation pipline has been built / trained
         self.assertEqual([['odor'], ['habitat']], map(lambda x: x[0], auto.input_mapper.built_features))
 
-    def test_transform(self):
-        test_df = self.mushroom_dataframe()
+    def test_transform_no_response(self):
+        train_df = self.mushroom_dataframe()
 
         # Two numerical variables, df_out = False
         test_columns = ['odor', 'habitat']
         auto = Automater(categorical_vars=test_columns, df_out=False)
-        auto.fit(test_df)
+        auto.fit(train_df)
 
-        (X,y) = auto.transform(test_df)
-        # TODO See what the correct shape is
+        (X,y) = auto.transform(train_df)
         self.assertEqual((8124, ), X[0].shape)
+        self.assertEqual(None, y)
 
         # Two numerical variables, df_out = True
         test_columns = ['odor', 'habitat']
         auto = Automater(categorical_vars=test_columns, df_out=True)
-        auto.fit(test_df)
+        auto.fit(train_df)
 
-        transformed = auto.transform(test_df)
-        # TODO See what correct shape and items are
+        transformed = auto.transform(train_df)
         self.assertEqual(8124, len(transformed.index))
         self.assertEqual((8124, 2), transformed.shape)
         self.assertItemsEqual(test_columns, transformed.columns)
 
+    def test_transform_with_response(self):
+        train_df = self.mushroom_dataframe()
+
+        # Two numerical variables, df_out = False
+        test_columns = ['odor', 'habitat']
+        auto = Automater(categorical_vars=test_columns, df_out=False, response_var='habitat')
+        auto.fit(train_df)
+
+        (X,y) = auto.transform(train_df)
+        self.assertEqual((8124, ), X[0].shape)
+
+        # Two numerical variables, df_out = True
+        test_columns = ['odor', 'habitat']
+        auto = Automater(categorical_vars=test_columns, df_out=True, response_var='habitat')
+        auto.fit(train_df)
+
+        transformed = auto.transform(train_df)
+        self.assertEqual(8124, len(transformed.index))
+        self.assertEqual((8124, 2), transformed.shape)
+        self.assertItemsEqual(test_columns, transformed.columns)
+
+        # TODO Test w/ response var unavailable.
+        test_columns = ['odor']
+        test_df = train_df[test_columns]
+        transformed = auto.transform(test_df)
+        self.assertEqual(8124, len(transformed.index))
+        self.assertEqual((8124, 1), transformed.shape)
+        self.assertItemsEqual(test_columns, transformed.columns)
+
     def test_create_input_nub_numerical(self):
-        test_df = self.mushroom_dataframe()
+        train_df = self.mushroom_dataframe()
 
         # Zero variables
         variable_type_dict = {'categorical_vars': []}
-        input_layers, input_nub = Automater()._create_input_nub(variable_type_dict, test_df)
+        input_layers, input_nub = Automater()._create_input_nub(variable_type_dict, train_df)
         self.assertEqual(list(), input_layers)
 
         # One variable
         iris_numerical_cols = ['odor']
         variable_type_dict = {'numerical_vars': iris_numerical_cols}
         input_layers, input_nub = Automater(numerical_vars=iris_numerical_cols).\
-            _create_input_nub(variable_type_dict, test_df)
+            _create_input_nub(variable_type_dict, train_df)
         # TODO Check layer type
         self.assertEqual(1, len(input_layers))
 
@@ -69,7 +97,7 @@ class TestCategorical(unittest.TestCase):
         iris_numerical_cols = ['odor', 'habitat', 'population']
         variable_type_dict = {'numerical_vars': iris_numerical_cols}
         input_layers, input_nub = Automater(numerical_vars=iris_numerical_cols).\
-            _create_input_nub(variable_type_dict, test_df)
+            _create_input_nub(variable_type_dict, train_df)
         self.assertEqual(3, len(input_layers))
 
     def test_numerical_whole(self):
