aefaf2f4914e854cd21735e9123455ea9dbfa099
Brendan Herger <13herger@gmail.com>
2018-05-25 19:47:46 -0700

    Modifying setup.py with descriptions

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Set up requirements
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
     name='keras-pandas',
-    description='test',
-    version='1.0.0',  # Update the version number for new releases
+    description='Easy and rapid deep learning',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='1.1.0',
     url='https://github.com/bjherger/keras-pandas',
     author='Brendan Herger',
     author_email='13herger@gmail.com',
     license='MIT',
-    packages=['keras_pandas'],
+    packages=find_packages(),
     install_requires=required,
     zip_safe=False
 )
