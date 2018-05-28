55b874ba3c76d8f00be25dcda54e617dfb745b67
Brendan Herger <13herger@gmail.com>
2018-05-28 15:30:43 -0700

    Forcing sphinx version to reference setup.py version

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from collections import OrderedDict
 
 from setuptools import setup, find_packages
 
-# Set up requirements
+# Version number
+version = '1.1.3'
 
+# Add README as long description
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Parse requirements.txt
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
@@ -15,7 +18,7 @@ setup(
     description='Easy and rapid deep learning',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='1.1.1',
+    version=version,
     url='https://github.com/bjherger/keras-pandas',
     author='Brendan Herger',
     author_email='13herger@gmail.com',
