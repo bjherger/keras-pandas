2dc31cac8f4319d8df8b901f928a5b23d0f3a60a
Brendan Herger <13herger@gmail.com>
2018-12-03 13:25:08 +0800

    Minor update to ts example

--- examples/README.md
+++ examples/README.md
@@ -19,4 +19,14 @@ Input data types: numerical, categorical, text
 
 Description: Predicting a user's dti (debt to income ratio) 
 
+## `instanbul_predict_ise.py`
+
+Problem type: Regression
+
+Input data types: numerical, timeseries
+
+Description: Using historical stock data and values for other stocks, to predict the value of a single stock (`ise`).
+ This script highlights the expected format for timeseries data. Timeseries input data should be an array of values 
+ (e.g. `[val1, val2, val3]`)
+
 
--- tests/testExamples.py
+++ tests/testExamples.py
@@ -1,4 +1,4 @@
-from examples import lending_club_predict_loan_status
+from examples import lending_club_predict_loan_status, lending_club_predict_dti, instanbul_predict_ise
 from tests.testbase import TestBase
 
 
@@ -6,3 +6,9 @@ class TestExamples(TestBase):
 
     def test_lending_club_predict_loan_status(self):
         lending_club_predict_loan_status.main()
+
+    def test_lending_club_predict_dti(self):
+        lending_club_predict_dti.main()
+
+    def test_instanbul_predict_ise(self):
+        instanbul_predict_ise.main()
